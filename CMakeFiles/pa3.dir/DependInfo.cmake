d8f42be876c73480ad576a688d1753f8f320e291
jettca <jettca1@gmail.com>
2014-04-09 22:16:20 -0400

    added cmake files

---
+++ CMakeFiles/pa3.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/jettca/Documents/School/Graphics/Homework/programming3/src/main.cpp" "/Users/jettca/Documents/School/Graphics/Homework/programming3/CMakeFiles/pa3.dir/src/main.cpp.o"
+  "/Users/jettca/Documents/School/Graphics/Homework/programming3/src/mesh.cpp" "/Users/jettca/Documents/School/Graphics/Homework/programming3/CMakeFiles/pa3.dir/src/mesh.cpp.o"
+  "/Users/jettca/Documents/School/Graphics/Homework/programming3/src/shaders.cpp" "/Users/jettca/Documents/School/Graphics/Homework/programming3/CMakeFiles/pa3.dir/src/shaders.cpp.o"
+  "/Users/jettca/Documents/School/Graphics/Homework/programming3/src/stb_image.cpp" "/Users/jettca/Documents/School/Graphics/Homework/programming3/CMakeFiles/pa3.dir/src/stb_image.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/System/Library/Frameworks/GLUT.framework/Headers"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ CMakeFiles/pa3.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/pa3.dir/src/main.cpp.o"
+  "CMakeFiles/pa3.dir/src/mesh.cpp.o"
+  "CMakeFiles/pa3.dir/src/shaders.cpp.o"
+  "CMakeFiles/pa3.dir/src/stb_image.cpp.o"
+  "pa3.pdb"
+  "pa3"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/pa3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
